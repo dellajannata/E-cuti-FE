457f3786717f745c9a1ba67acad82a1d863a5301
Ifa Indrian <ifaindrian152@gmail.com>
2023-08-14 11:10:00 +0700

    update search

--- components/data_by_status/PengajuanCutiAccKabid.vue
+++ components/data_by_status/PengajuanCutiAccKabid.vue
@@ -58,7 +58,7 @@ export default {
         axios.get(`http://127.0.0.1:8000/api/pengajuan_cuti/search/${this.searchQuery}`)
           .then(res => {
             console.log(res.data.data);
-            this.data_cuti = res.data.data;
+            this.data_cuti = res.data.data.filter(cuti => cuti.status === "Belum");
           })
           .catch(error => {
             console.error('Error fetching data:', error);
