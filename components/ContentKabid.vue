910d8cc207bc88072c57489f036339c3fa2670ac
Ifa Indrian <ifaindrian152@gmail.com>
2023-09-20 13:21:02 +0700

    dashboard kabid

--- components/ContentKabid.vue
+++ components/ContentKabid.vue
@@ -13,90 +13,120 @@
         </div>
       </div>
     </div>
-  
-      <!-- Rekap Cuti -->
-      <div class="col-lg-3 col-sm-6">
-        <div class="card1">
-          <div class="card-body">
-            <h4 class="card-title">Rekap Cuti</h4>
-            <div class="d-inline-block">
-              <h2>{{ rekap_cuti.length }}</h2>
-              <NuxtLink to="/rekap_cuti_acc_kabid" class="small-box-footer">Selengkapnya <i class="fa fa-arrow-right"></i></NuxtLink>
-            </div>
-            <span class="float-right display-5 opacity-5"><i class="mdi mdi-animation"></i></span>
+
+    <!-- Rekap Cuti -->
+    <div class="col-lg-3 col-sm-6">
+      <div class="card1">
+        <div class="card-body">
+          <h4 class="card-title">Rekap Cuti</h4>
+          <div class="d-inline-block">
+            <h2>{{ rekap_cuti.length }}</h2>
+            <NuxtLink to="/rekap_cuti_acc_kabid" class="small-box-footer">Selengkapnya <i class="fa fa-arrow-right"></i></NuxtLink>
           </div>
+          <span class="float-right display-5 opacity-5"><i class="mdi mdi-animation"></i></span>
         </div>
       </div>
-      <!-- User -->
-      <!-- Halo, {{ userLoggedin?.name }} -->
+    </div>
+    <!-- User -->
+    <!-- Halo, {{ userLoggedin?.name }} -->
   </div>
 </template>
+
 <script>
-  import axios from 'axios';
-  export default {
-    data() {
-      return {
-        cuti_kabid: [],
-        rekap_cuti: [],
-        data_pegawai: []
-      }
-    },
-    mounted() {
-      this.getDataRekapCuti();
-      this.getDataCutiKabid();
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      cuti_kabid: [],
+      rekap_cuti: [],
+      data_pegawai: [],
+      userLoggedin: null,
+    };
+  },
+  created() {
+    this.getUserData();
+  },
+  methods: {
+    getUserData() {
+      const user = JSON.parse(localStorage.getItem('user'));
+      this.userLoggedin = user;
       this.getDataPegawai();
     },
-    computed: {
-      getUserUnit() {
-        const kabidId = JSON.parse(localStorage.getItem('user')).pegawai_id
-        if (this.data_pegawai.length) {
-          const filtering = JSON.parse(JSON.stringify(this.data_pegawai.find(item => item.id === kabidId)))
-          console.log(filtering.unit_kerja)
-          return filtering.unit_kerja
-        }
-      },
+    getDataCutiKabid() {
+      const accessToken = localStorage.getItem('token');
+      const unitKerja = this.getUnitKerjaPegawai();
+
+      axios
+        .get('http://127.0.0.1:8000/api/pengajuan_cuti_acc_kabid', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data.data);
+          this.cuti_kabid = res.data.data.filter(
+            (cuti_kabid) => cuti_kabid.pegawai.unit_kerja === unitKerja && cuti_kabid.status === 'Belum'
+          );
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
     },
-    methods: {
-      getDataCutiKabid() {
-        const accessToken = localStorage.getItem('token');
-        axios.get('http://127.0.0.1:8000/api/pengajuan_cuti_acc_kabid', {
+    getDataRekapCuti() {
+      const accessToken = localStorage.getItem('token');
+      const unitKerja = this.getUnitKerjaPegawai();
+
+      axios
+        .get('http://127.0.0.1:8000/api/pengajuan_cuti_all', {
           headers: {
-            'Authorization': `Bearer ${accessToken}`
-          }
-        }).then(res => {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((res) => {
           console.log(res.data.data);
-          this.cuti_kabid = res.data.data.filter(cuti_kabid => cuti_kabid.pegawai.unit_kerja === this.getUserUnit & cuti_kabid.status === "Belum");
-          console.log(this.getUserUnit);
-        }).catch(error => {
+          this.rekap_cuti = res.data.data.filter(
+            (rekap_cuti) => rekap_cuti.pegawai.unit_kerja === unitKerja && rekap_cuti.status === 'Selesai'
+          );
+        })
+        .catch((error) => {
           console.error('Error fetching data:', error);
         });
-      },
-      getDataRekapCuti() {
-        const accessToken = localStorage.getItem('token');
-        axios.get('http://127.0.0.1:8000/api/pengajuan_cuti_all', {
+    },
+    getDataPegawai() {
+      const accessToken = localStorage.getItem('token');
+      axios
+        .get('http://127.0.0.1:8000/api/pegawai_all', {
           headers: {
-            'Authorization': `Bearer ${accessToken}`
-          }
-        }).then(res => {
-          console.log(res.data.data);
-          this.rekap_cuti = res.data.data.filter(rekap_cuti => rekap_cuti.pegawai.unit_kerja === this.getUserUnit & rekap_cuti.status === "Selesai");
-        }).catch(error => {
-          console.error('Error fetching data:', error);
-        });
-      },
-      getDataPegawai() {
-        const accessToken = localStorage.getItem('token');
-        axios.get('http://127.0.0.1:8000/api/pegawai_all', {
-          headers: {
-            'Authorization': `Bearer ${accessToken}`
-          }
-        }).then(res => {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((res) => {
           console.log(res.data.data);
           this.data_pegawai = res.data.data;
-        }).catch(error => {
+          this.getDataCutiKabid(); 
+          this.getDataRekapCuti(); 
+        })
+        .catch((error) => {
           console.error('Error fetching pegawai data:', error);
         });
-      },
-    }
-  };
-  </script>
+    },
+    getPegawaiId() {
+      const userData = JSON.parse(localStorage.getItem('user'));
+      return userData ? userData.pegawai_id : ''; // Mengambil id user dari objek pengguna
+    },
+    getUnitKerjaPegawai() {
+      const user = this.getPegawaiId();
+      const pegawai = this.data_pegawai.find((pegawai) => pegawai.id === user);
+
+      if (pegawai) {
+        console.log('Unit Kerja Pegawai:', pegawai.unit_kerja);
+        return pegawai.unit_kerja;
+      } else {
+        console.log('Unit Kerja Pegawai Tidak Tersedia');
+        return 'Unit Kerja Pegawai Tidak Tersedia';
+      }
+    },
+  },
+};
+</script>
